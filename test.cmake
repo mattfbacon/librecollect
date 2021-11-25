02f7a80ea4594defd2dd0721ccaa98415e2ae4f0
Matt Fellenz <mattf53190@gmail.com>
2021-11-24 19:59:10 -0800

    Update CMakeLists.txt and test.cmake

--- test.cmake
+++ test.cmake
@@ -1,21 +1,18 @@
 set(DIR_TEST test)
 
 set(test_sources
-	main.cpp
 	Tests/behavior.cpp
 	Tests/flush.cpp
 	Tests/get_set_clear_has_delete.cpp
 	Tests/handle.cpp
 	Tests/increment_decrement.cpp
+	Tests/instance.hpp
 	Tests/prepend_append.cpp
+	main.cpp
 	)
-list(TRANSFORM test_sources PREPEND ${${PROJECT_NAME}_SOURCE_DIR}/${DIR_TEST}/)
-add_executable(test ${test_sources})
+list(TRANSFORM test_sources PREPEND ${DIR_TEST}/)
+
+add_executable(tests ${test_sources})
 
 find_package(Catch2 2 REQUIRED)
-target_link_libraries(test PRIVATE Catch2::Catch2 ${EXECUTABLE_NAME})
-target_include_directories(test PRIVATE ${DIR_TEST})
-
-set_property(TARGET test PROPERTY RUNTIME_OUTPUT_DIRECTORY "${${PROJECT_NAME}_SOURCE_DIR}/${DIR_DIST}")
-set_property(TARGET test PROPERTY LIBRARY_OUTPUT_DIRECTORY "${${PROJECT_NAME}_SOURCE_DIR}/${DIR_DIST}")
-set_property(TARGET test PROPERTY ARCHIVE_OUTPUT_DIRECTORY "${${PROJECT_NAME}_SOURCE_DIR}/${DIR_DIST}")
+target_link_libraries(tests PRIVATE Catch2::Catch2 ${EXECUTABLE_NAME})
